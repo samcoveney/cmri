b912263dd0f823904bb83a0dd70980ea4509a5ae
Sam Coveney <s.coveney@leeds.ac.uk>
2022-05-27 15:57:36 +0100

    fixed bugs

--- cmri/reg.py
+++ cmri/reg.py
@@ -1,8 +1,11 @@
 """Registration."""
 
 import numpy as np
+import numbers
+import time
 import SimpleITK as sitk
 
+import cmri.utils as utils
 
 def affine_registration_sitkimage(moving, static,
                         pipeline=None,
@@ -62,9 +65,6 @@ def affine_registration_sitkimage(moving, static,
     # it always seems to reach this... so not converging
     if iterations is not None:
         elastixImageFilter.SetParameter('MaximumNumberOfIterations', str(int(iterations)))
-
-    #import IPython
-    #IPython.embed()
 
     if not(verbose):
         elastixImageFilter.LogToConsoleOff()
@@ -147,4 +147,123 @@ def affine_transform(image, params):
 
     return transformed
 
+
+def register_series(series, ref, pipeline=None, denoise=True, static_mask=None, verbose=False, iterations=None):
+    """Register a series to a reference image.
+
+    Parameters
+    ----------
+
+    series : NumPy array 
+    ref : int, or NumPy array
+    pipeline : list
+    denoise : bool
+    static_mask : NumPy array
+    verbose : bool, default False
+    iterations : int, default None
+
+    Returns
+    -------
+
+    """
+
+    # for collecting results
+    xformed = np.zeros(series.shape)
+
+    # convert NumPy array into ITK image series
+    series = sitk.GetImageFromArray(series, isVector=True)
+    
+    if static_mask is not None:
+        static_mask = sitk.GetImageFromArray(static_mask)
+        static_mask = sitk.Cast(static_mask, sitk.sitkUInt8)
+
+    filt2D = sitk.VectorIndexSelectionCastImageFilter()
+    num_acq = series.GetNumberOfComponentsPerPixel()
+
+    if isinstance(ref, numbers.Number):
+        ref_as_idx = ref
+        filt2D.SetIndex(ref)
+        ref = filt2D.Execute(series)
+    else:
+        ref_as_idx = False
+        ref = sitk.GetImageFromArray(ref)
+
+    # denoise ref image
+    if denoise:
+        denoiseFilter = sitk.PatchBasedDenoisingImageFilter()
+        ref = denoiseFilter.Execute(ref)
+
+    time_per_run = 0.0
+    time_left = "??:??:??"
+    for ii in range(num_acq):
+        start_time = time.time()
+        utils.progress_bar(ii, num_acq, prefix = '', suffix = ' ' + time_left, decimals = 0, length = 20, fill = '#')
+
+        filt2D.SetIndex(ii)
+        this_moving = filt2D.Execute(series)
+        if isinstance(ref_as_idx, numbers.Number) and ii == ref_as_idx:
+            # This is the reference! No need to move and the xform is I(4):
+            xformed[..., ii] = sitk.GetArrayFromImage(this_moving)
+        else:
+
+            # denoise moving image
+            if denoise:
+                this_moving_denoised = denoiseFilter.Execute(this_moving)
+            else:
+                this_moving_denoised = this_moving
+
+            transformed, params = affine_registration_sitkimage(
+                this_moving_denoised, ref,
+                pipeline=pipeline,
+                static_mask=static_mask,
+                verbose=verbose,
+                iterations=iterations)
+
+            # apply transformation to non-denoised image
+            if denoise:
+                transformed = sitk.Transformix(this_moving, params)
+
+            xformed[..., ii] = sitk.GetArrayFromImage(transformed)
+
+            time_per_run = time_per_run + (time.time() - start_time - time_per_run) / (ii + 1) 
+            time_left = time.strftime('%H:%M:%S', time.gmtime((num_acq - ii - 1)*time_per_run))
+
+    return xformed
+
+
+def register_dwi_series(data, gtab, b0_ref=0, pipeline=None, denoise=True,
+                        static_mask=None, verbose=False, iterations=None):
+    """Register a DWI series to the mean of the B0 images in that series.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
+
+    # First, register the b0s to one image and average
+    if np.sum(gtab.b0s_mask) > 1:
+        b0_img = data[..., gtab.b0s_mask]
+        trans_b0 = register_series(b0_img, ref=b0_ref,
+                                   pipeline=pipeline,
+                                   denoise=denoise,
+                                   static_mask=static_mask,
+                                   verbose=verbose,
+                                   iterations=iterations)
+        ref_data = np.mean(trans_b0, -1)
+    else:
+        trans_b0 = data[..., gtab.b0s_mask]
+        ref_data = np.squeeze(trans_b0, axis=-1)
+
+    # Second, register all images to ref_data  
+    xformed = register_series(data, ref_data,
+                              pipeline=pipeline,
+                              denoise=denoise,
+                              static_mask=static_mask,
+                              verbose=verbose,
+                              iterations=iterations)
+
+    return xformed 
 
--- cmri/roi.py
+++ cmri/roi.py
@@ -1,6 +1,5 @@
 """Region Of Interest."""
 
-import sys
 import numpy as np
 from scipy.spatial.distance import cdist
 
@@ -17,35 +16,40 @@ from matplotlib.path import Path
 from scipy import interpolate
 
 from cmri import utils
+from cmri.plot import Multiplot
 
 
-class create_mask:
+class Select_mask(Multiplot):
     """Define a mask.
 
        https://stackoverflow.com/questions/68229211/matplotlib-ellipseselector-how-to-get-the-path
     """
 
-    def __init__(self, img):
+    def __init__(self, scalars, evecs=None, evals=None):
+        super().__init__(scalars, evecs, evals)
 
-        # get pixel coordinates
-        self.img_shape = img.shape
-        X, Y = np.mgrid[0:img.shape[0], 0:img.shape[1]]
-        self.MYDATA = np.vstack([X.ravel(), Y.ravel()])
+    def run(self):
 
-        # create figure and selector
-        self.fig, self.ax = plt.subplots()
-        self.ax.imshow(img.T, cmap = "gray")
         self.es = EllipseSelector(self.ax, self.onselect,
-                                  interactive=True)
-        plt.title("Click & drag, adjust, close plot.")
+                                  interactive=True, props={"fill": False},
+                                  drag_from_anywhere=True)
+        self.ax.set_title("Click & drag, adjust, close window.")
         plt.show()
+
+        return self.mask
 
     # selector callback method
     def onselect(self, eclick, erelease):
         ext = self.es.extents
         ellipse = Ellipse(self.es.center, ext[1]-ext[0], ext[3]-ext[2])
-        mask = ellipse.contains_points(self.MYDATA.T)
-        self.mask = mask.reshape(self.img_shape).astype(np.int32)
+        mask = ellipse.contains_points(self.coors)
+        self.mask = mask.reshape(self.Nx, self.Ny).astype(np.int32)
+        alpha = (self.mask + 0.3) / (1.3) 
+
+        if (self.evecs is not None) and (self.evals is not None): 
+            self.ax.collections[0].set_alpha(alpha)
+        else:
+            self.im.set_alpha(alpha.T)
 
 
 def square_from_mask(mask, return_indices=False):
@@ -64,112 +68,6 @@ def square_from_mask(mask, return_indices=False):
     else:
         return x_tmp[0], x_tmp[-1] + 1, y_tmp[0], y_tmp[-1] + 1
 
-
-class Multiplot(object):
-    """Plotter for multiple images of the same size."""
-
-    def __init__(self, scalars, evecs=None, evals=None):
-
-        self.evecs, self.evals, self.scalars =\
-        evecs, evals, scalars
-
-        [self.Nx, self.Ny] = self.scalars[0]["values"].shape 
-        x, y = np.mgrid[:self.Nx, :self.Ny]
-        self.coors = np.hstack((x.reshape(-1, 1), y.reshape(-1,1)))
-
-        # store the color info here
-        for idx in range(len(self.scalars)):
-            scalars = self.scalars[idx]["values"]
-            name = self.scalars[idx]["name"]
-            cmap, vmin, vmax = utils.get_colors(name)
-            self.scalars[idx]["cmap"] = cmap
-            if vmin is None: vmin = scalars.min()
-            if vmax is None: vmax = scalars.max()
-            self.scalars[idx]["vmin"] = vmin
-            self.scalars[idx]["vmax"] = vmax
-            self.scalars[idx]["vdif"] = vmax - vmin 
-
-        # which color
-        self.cidx = 0
-
-        # setup plot
-        plt.style.use('dark_background')
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot()
-
-        self.fig.canvas.draw_idle()  # NOTE: perhaps pointless, draw happens in update
-        
-        if (self.evecs is not None) and (self.evals is not None): 
-            self.ax, self.cbar = tensor_plot_2d(self.evecs, self.evals, self.scalars[self.cidx]["values"], self.scalars[self.cidx]["name"], self.ax)
-        else:
-            cmap, vmin, vmax = utils.get_colors(self.scalars[self.cidx]["name"])
-            self.im = self.ax.imshow(self.scalars[self.cidx]["values"].T, cmap=cmap, vmin=vmin, vmax=vmax)
-            self.cbar = plt.colorbar(self.im, ax=self.ax)
-            self.cbar.set_label(self.scalars[self.cidx]["name"])
-
-        self.cbar.set_label(self.scalars[self.cidx]["name"])
-        self.fig.canvas.mpl_connect('key_press_event', self.key_press)
-
-
-    def key_press(self, event):
-        """Changes scalars and color limits in the plot."""
-        print('press:', event.key)
-
-        sys.stdout.flush()
-        if event.key == " ":
-            print("pressed space")
-            self.cidx += 1
-            if self.cidx == len(self.scalars):
-                self.cidx = 0
-            
-            scalars = self.scalars[self.cidx]["values"].flatten()
-            name = self.scalars[self.cidx]["name"]
-            self.cbar.set_label(self.scalars[self.cidx]["name"])
-            cmap = self.scalars[self.cidx]["cmap"]
-            vmin = self.scalars[self.cidx]["vmin"]
-            vmax = self.scalars[self.cidx]["vmax"]
-
-            if (self.evecs is not None) and (self.evals is not None): 
-                self.ax.collections[0].set_array(scalars)
-                self.ax.collections[0].set_clim(vmin, vmax)
-                self.ax.collections[0].set_cmap(cmap)
-            else:
-                self.im.set_data(scalars.reshape(self.Nx, self.Ny).T)
-                self.im.set_clim(vmin, vmax)
-                self.im.set_cmap(cmap)
-       
-            self.fig.canvas.draw_idle()
-            
-        if event.key in ["left", "right", "down", "up"]:
-            vmin = self.scalars[self.cidx]["vmin"]
-            vmax = self.scalars[self.cidx]["vmax"]
-            vdif = self.scalars[self.cidx]["vdif"]
-            factor = 0.05
-            if event.key == "left":
-                vmin = vmin - factor*vdif
-            if event.key == "right":
-                vmin = vmin + factor*vdif
-            if event.key == "down":
-                vmax = vmax - factor*vdif
-            if event.key == "up":
-                vmax = vmax + factor*vdif
-
-            if vmax > vmin:
-                self.scalars[self.cidx]["vmin"] = vmin
-                self.scalars[self.cidx]["vmax"] = vmax
-
-            if (self.evecs is not None) and (self.evals is not None): 
-                self.ax.collections[0].set_clim(vmin, vmax)
-            else:
-                self.im.set_clim(vmin, vmax)
-
-            self.fig.canvas.draw_idle()
-
-    def run(self):
-        """Just plotting"""
-
-        plt.show()
-    
 
 class Select_point(Multiplot):
     """Select a coordinate by clicking on an image."""
@@ -202,144 +100,6 @@ class Select_point(Multiplot):
 
         self.x, self.y = event.xdata, event.ydata
         self.fig.canvas.draw()
-
-
-class select_outliers:
-    """https://stackoverflow.com/questions/47439355/draggable-lines-select-one-another-in-matplotlib"""
-    def __init__(self, data, data_f, MI, bvals=None):
-
-        self.data = data
-        self.data_f = data_f
-        self.midx = np.argsort(MI)
-        self.MIS = MI[self.midx]
-        self.XorY = self.MIS.mean()
-
-        # create grid of plots
-        fig = plt.figure(tight_layout=True)
-        gs = gridspec.GridSpec(3, 2)
-
-        # plot for MI
-        self.ax = fig.add_subplot(gs[0, :])
-        if bvals is None:
-            self.ax.plot(self.MIS, marker='o', c='black')
-        else:
-            self.ax.plot(self.MIS, c='black')
-            self.ax.scatter(np.arange(data.shape[-1]), self.MIS, c=bvals)
-        self.ax.set_ylabel("good ---> bad")
-        self.c = self.ax.get_figure().canvas
-
-        # plots for images
-        self.axg = fig.add_subplot(gs[1, 0])
-        self.axb = fig.add_subplot(gs[1, 1])
-        self.axg_f = fig.add_subplot(gs[2, 0])
-        self.axb_f = fig.add_subplot(gs[2, 1])
-        self.axg.set_title("good obs")
-        self.axg_f.set_title("good pred")
-        self.axb.set_title("bad obs")
-        self.axb_f.set_title("bad pred")
-        for a in [self.axg, self.axg_f, self.axb, self.axb_f]:
-            a.set_axis_off()
-        self.update_plots()
-
-        # line for threshold
-        x = [0, data.shape[-1]]
-        y = [self.XorY, self.XorY]
-        self.line = lines.Line2D(x, y, picker=5)
-        self.ax.add_line(self.line)
-        self.c.draw_idle()
-        self.releaser = self.c.mpl_connect("button_press_event", self.releaseonclick)
-
-    def releaseonclick(self, event):
-        self.line.set_ydata([event.ydata, event.ydata])
-        self.XorY = self.line.get_ydata()[0]
-        self.c.draw_idle()
-        self.update_plots()
-
-    def update_plots(self):
-        im_good = self.data[:, :, self.midx[self.MIS < self.XorY]][..., -1]
-        im_good_f = self.data_f[:, :, self.midx[self.MIS < self.XorY]][..., -1]
-        vming = np.min([im_good.min(), im_good_f.min()])
-        vmaxg = np.max([im_good.max(), im_good_f.max()])
-        im_bad = self.data[:, :, self.midx[self.MIS > self.XorY]][..., 0]
-        im_bad_f = self.data_f[:, :, self.midx[self.MIS > self.XorY]][..., 0]
-        vminb = np.min([im_bad.min(), im_bad_f.min()])
-        vmaxb = np.max([im_bad.max(), im_bad_f.max()])
-        self.axg.imshow(im_good.T, cmap="turbo", vmin=vming, vmax=vmaxg)
-        self.axg_f.imshow(im_good_f.T, cmap="turbo", vmin=vming, vmax=vmaxg)
-        self.axb.imshow(im_bad.T, cmap="turbo", vmin=vminb, vmax=vmaxb)
-        self.axb_f.imshow(im_bad_f.T, cmap="turbo", vmin=vminb, vmax=vmaxb)
-
-        self.c.draw_idle()
-
-    def run(self):
-        plt.show()
-
-        good_img = np.ones(self.data.shape[-1])
-        bad = self.midx[self.MIS > self.XorY]
-        good_img[bad] = 0
-
-        return good_img
-
-
-def ellipsoid_shadows(evecs, evals):
-    """Get 2d shadow of ellipsed onto xy plane"""
-
-    # NOTE: attempt a 2D tensor plot with ellipses
-    [Y, X] = np.meshgrid(range(evecs.shape[1]), range(evecs.shape[0]))
-    XY = np.column_stack((X.ravel(), Y.ravel()))
-
-    # equivalent to using 'norm' in fury plot
-    Qn = evals / np.max(evals, axis = 3)[..., None] #[:, :, 0, 0:2])
-
-    D = np.zeros([evecs.shape[0], evecs.shape[1], 3, 3])
-    P = np.array([[1,0,0], [0,1,0], [0,0,0]])
-    Q = np.zeros([evecs.shape[0], evecs.shape[1], 2])
-    V = np.zeros([evecs.shape[0], evecs.shape[1], 2, 2])
-    for i in range(Q.shape[0]):
-        for j in range(Q.shape[1]):
-            tmp = evecs[i, j, 0] @ np.diag(Qn[i, j, 0]) @ evecs[i, j, 0].T
-            tmp = P.T @ tmp @ P
-            q, v = np.linalg.eigh(tmp[0:2, 0:2])  # note: ascending order
-            Q[i, j] = q
-            V[i, j] = v
-
-    aa = np.degrees(np.arctan(V[..., -1, 1] / V[..., -1, 0]))
-    ww, hh = Q[:, :, 1], Q[:, :, 0]
-
-    return XY, ww, hh, aa
-
-
-def tensor_plot_2d(evecs, evals, scalars, color, ax=None):
-    """2d plot of tensors"""
-
-    cmap, vmin, vmax = utils.get_colors(color)
-
-    if ax is None:
-        plt.style.use('dark_background')
-        fig, ax = plt.subplots()
-        make_plot = True
-    else:
-        make_plot = False
-
-    XY, ww, hh, aa = ellipsoid_shadows(evecs, evals)
-    ec = EllipseCollection(ww, hh, aa, units='xy', offsets=XY,  # NOTE: 'xy' may be bad as angle depends on aspect ratio
-                           transOffset=ax.transData,
-                           cmap=cmap, clim=[vmin, vmax], array=scalars.flatten())
-
-    ax.add_collection(ec)
-    ax.autoscale_view()
-    ax.set_xlim(-0.5, evecs.shape[0]-0.5)
-    ax.set_ylim(-0.5, evecs.shape[1]-0.5)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    cbar = plt.colorbar(ec)
-    cbar.set_label('scalars')
-    ax.set_aspect('equal')
-
-    if make_plot:
-        plt.show()
-    else:
-        return ax, cbar
 
 
 class Segment_surfs(Multiplot):
@@ -590,8 +350,9 @@ class Segment_aha(Multiplot):
         angles = np.rad2deg(angles)
         angles[angles < 0] = 360 + angles[angles < 0] # adjust range to 0..360
 
-        # NOTE: return the segment labels 
-        labels = np.ceil(angles / (360 / self.segs))
+        # return the segment labels 
+        labels = np.ceil(angles / (360 / self.segs)) + 1
+        labels[labels > self.segs] = 1
 
         return labels.reshape(self.Nx, self.Ny).astype(int)
 
